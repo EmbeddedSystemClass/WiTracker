bd166b998b0edfbae4afa160df5943d862dc4ca1
Alex Subaric <deadmeu@gmail.com>
2018-05-17 19:08:11 +1000

    Updated main app's program flow

--- src/mc-esp32/main/app_main.c
+++ src/mc-esp32/main/app_main.c
@@ -8,6 +8,7 @@
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_event_loop.h"
+#include "esp_pm.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -24,35 +25,121 @@
 
 #include "mqtt.h"
 #include "wifi.h"
+#include "time.h"
 
-static void hardware_init(void);
+typedef struct {
+    char* wifiScanResult;
+} Device_Data;
+
+#define SLEEP_MODE_LIGHT_LENGTH 30000
+
+static const char *TAG = "MAIN_APP"; // logging tag
+
+/* Variable holding number of times ESP32 restarted since first boot.
+ * It is placed into RTC memory using RTC_DATA_ATTR and
+ * maintains its value when ESP32 wakes from deep sleep.
+ */
+RTC_DATA_ATTR static int bootCount = 0;
+
+static Device_Data data;  // This should be a custom struct type
+
+static void program_init(void);
+void collection_preprocessor(void);
+void collect_data(void);
+void handle_data(void);
+void enter_sleep(void);
 
 void app_main(void)
 {
-    hardware_init();
+    program_init();
 
     while (1) {
-        char *scanString = mc_wifi_scan();
-        if (scanString == NULL) {
-            vTaskDelay(10000 / portTICK_PERIOD_MS);
-            continue;
-        }
+        collection_preprocessor();
+        
+        // Active scan mode
+        collect_data();
+        update_time();
+        handle_data();
 
-        printf("%s\n", scanString);
-
-        if (mc_mqtt_publish(scanString) == -1) {
-            printf("ERROR: MQTT COULDNT CONNECT!\n");
-        }
-
-        free(scanString);
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        // Enter light/deep sleep mode
+        enter_sleep();
     }
 }
 
-void hardware_init(void)
+// TODO: add function comments
+void collection_preprocessor(void) {
+    // check if test switch is set
+    // check if deep sleep switch is set
+    printf("It's loop time!\n");
+}
+
+// Reads and stores results in data variables
+void collect_data(void) {
+    esp_wifi_start();
+    data.wifiScanResult = mc_wifi_scan();
+    // data.wifiScanResult = NULL;
+}
+
+// Processes and uploads data to the MQTT datastore
+void handle_data(void) {
+    if (data.wifiScanResult == NULL)
+    {
+        return;
+    }
+
+    if (mc_mqtt_publish(data.wifiScanResult) == -1)
+    {
+        printf("ERROR: MQTT COULDNT CONNECT!\n");
+    }
+
+    free(data.wifiScanResult);
+}
+
+// Enters an appropriate sleep mode
+void enter_sleep(void) {
+    printf("Entering light sleep for 10 seconds...\n");
+    fflush(NULL);
+    vTaskDelay(SLEEP_MODE_LIGHT_LENGTH / portTICK_PERIOD_MS);
+}
+
+// Initialises the hardware and program variables
+void program_init(void)
 {
-    nvs_flash_init();
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+    
+    bootCount++;
+    ESP_LOGI(TAG, "Boot count: %d", bootCount);
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+#if CONFIG_PM_ENABLE
+    // Configure dynamic frequency scaling: maximum frequency is set in sdkconfig,
+    // minimum frequency is XTAL.
+    rtc_cpu_freq_t max_freq;
+    rtc_clk_cpu_freq_from_mhz(CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ, &max_freq);
+    esp_pm_config_esp32_t pm_config = {
+        .max_cpu_freq = max_freq,
+        .min_cpu_freq = RTC_CPU_FREQ_XTAL};
+    ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
+#endif // CONFIG_PM_ENABLE
 
     mc_wifi_init();
-    mc_mqtt_init();
+
+    mc_mqtt_init();     // must be done after wifi init
+    mc_time_init();
 }
