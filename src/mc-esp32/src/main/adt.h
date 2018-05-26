a8543f90e213f912e17189d0ff1449c39b8e9642
Alex Subaric <deadmeu@gmail.com>
2018-05-27 06:08:06 +1000

    Started implementing packet sending/queueing

--- src/mc-esp32/src/main/adt.h
+++ src/mc-esp32/src/main/adt.h
@@ -1,7 +1,8 @@
 #ifndef ADT_H
 #define ADT_H
 
-typedef struct ADT_Node {
+typedef struct ADT_Node
+{
     struct ADT_Node *next;
     void *item;
 } ADT_Node;
--- src/mc-esp32/src/main/network.h
+++ src/mc-esp32/src/main/network.h
@@ -25,6 +25,15 @@ typedef struct
 
 typedef ADT_Node Packet;
 
+#define PACKET_OBJECT_SEPARATOR ","
+#define PACKET_START_CHAR "$"
+#define PACKET_ID_CHARS "GP03"
+#define PACKET_CHECKSUM_CHAR "*"
+#define PACKET_END_CHARS "\r\n"
+#define MAX_PACKETS_IN_SINGLE_UPLOAD 10
+
+extern void mc_network_init(void);
 extern void mc_network_transmit(Device_Data data);
+extern void mc_network_upload(void);
 
 #endif /* NETWORK_H */
